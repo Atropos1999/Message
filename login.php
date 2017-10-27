c1f3ec1e373666e616eac7afb50c8bd0521f0865
Atropos1999 <1181849919@qq.com>
2017-10-27 12:37:19 +0800

    update login page

--- login.php
+++ login.php
@@ -28,13 +28,22 @@
     <head>
         <meta charset="utf-8"/>
         <title>管理员登录</title>
+        <style>
+            .login{margin-top: 80px;text-align:center;}
+            .un{margin-top: 20px;height:30px;width:20%;background:#fff;}
+            .pw{margin-top: 20px;height:30px;width:20%;background:#fff;}
+            .button{margin-top: 20px;height:30px;width:10%;background:#fff;}
+            h1{font-family: Comic Sans MS;text-align:center;}
+        </style>
+
+        <h1>Login</h1>
     </head>
     <body background="timg.jpg">
         <form action="login.php?act=chk" method="post" style="width:100%;">
-            <div class="login" style="margin-top: 100px;text-align:center;">
-                <input style="margin-top: 20px;height:30px;width:20%;background:#fff;" type="text" name="username" autocomplete="off"/><br>
-                <input style="margin-top: 20px;height:30px;width:20%;background:#fff;" type="password" name="password" /><br>
-                <input style="margin-top: 20px;height:30px;width:20%;background:#fff;" type="submit" value="点击登录"/>
+            <div class="login">
+                <input class="un"  type="text" name="username" autocomplete="off" placeholder="管理员名"/><br>
+                <input class="pw"  type="password" name="password" placeholder="密码"/><br>
+                <input class="button" type="submit" value="点击登录"/>
             </div>
 
 
